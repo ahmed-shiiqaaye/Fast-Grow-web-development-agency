5d38813e4e8eb937bcb81fc950fb133f68f17065
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-04-18 20:36:10 +0200

    new addition of work exp

--- js/app.js
+++ js/app.js
@@ -10,3 +10,32 @@ menuBar.onclick = () =>{
         menuBar.innerHTML = `<i class="fa fa-bars"></i>`
     }
 }
+
+// btns filter
+let btnFilters = document.querySelector('.btns');
+let filterCard = document.querySelectorAll('.cardData');
+
+
+window.onload = () =>{
+    btnFilters.onclick = (e) =>{
+        let selected = e.target;
+        if(selected.classList.contains('btnFilter')){
+            btnFilters.querySelector('.active').classList.remove('active');
+            selected.classList.add('active');
+            let filterName = selected.getAttribute('data-name');
+            console.log(filterName)
+
+            filterCard.forEach((card)=>{
+                let filterData = card.getAttribute('data-name');
+                if(filterData == filterName || filterName == 'all'){
+                    card.classList.add('show')
+                    
+                }else{
+                    card.classList.add('hide')
+                    card.classList.remove('show');
+                }
+            })
+        }
+    }
+    
+}
